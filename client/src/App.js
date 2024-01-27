29f5a66f60a206a7b63c8670b5f5731822a9ccbf
Akash-akp <akashakp0037@gmail.com>
2024-01-27 16:41:27 +0530

    create header

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,22 @@ import About from './Pages/About';
 import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
 import Dashboard from './Pages/Dashboard';
+import Header from './Components/Header';
+import Project from './Pages/Project';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/project' element={<Project />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,97 @@
+import { React , useState } from "react";
+import { Link , NavLink , useLocation } from "react-router-dom"
+import { IoMdMoon } from "react-icons/io";
+import { MdSunny } from "react-icons/md";
+import { CiSearch } from "react-icons/ci";
+
+const Header = () => {
+    const location = useLocation();
+    const [ darkMode, setDarkMode ] = useState(false);
+
+    const navLinkClickHandler = (path) =>{
+        return location.pathname===path?('block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'):("block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700");
+    }
+
+    const darkModeHandler = () => {
+        if(darkMode){
+            document.body.classList.remove('dark');
+        }else{
+            document.body.classList.add('dark')
+        }
+        setDarkMode(!darkMode);
+    }
+
+    const searchHandler = (event) => {
+        event.preventDefault();
+    }
+
+  return (
+    <div className="">
+      <nav className="bg-white dark:bg-gray-900 relative w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <Link to='/'>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                EchoBlog
+                </span>
+            </Link>
+            <form onSubmit={searchHandler} className="flex items-center">   
+                <div className="relative lg:w-[200px] w-[40px]">
+                    <input 
+                    type="text" 
+                    className="bg-gray-50 border border-gray-500 text-gray-900 text-sm rounded-full lg:rounded-lg focus:ring-blue-500 focus:border-blue-500 hidden lg:block lg:w-[180px]  lg:pl-3 lg:pr-8 py-2 dark:bg-gray-700 dark:border-gray-600 placeholder:text-transparent lg:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    placeholder="Search..." 
+                    />
+                    <div className="lg:hidden h-[30px] w-[30px] border border-gray-300 rounded-full">
+
+                    </div>
+                    {/* Search Icon */}
+                    <button type="button" className="absolute text-lg lg:right-7 lg:top-[10px] right-[16px] top-[6px] text-gray-700 dark:text-gray-400 flex items-center">
+                        <CiSearch />
+                    </button>
+                </div>
+            </form>
+          <div className="flex items-center md:order-2 space-x-3 md:space-x-4 rtl:space-x-reverse">
+            <button onClick={darkModeHandler} className=" h-[30px] w-[30px] flex justify-center items-center rounded-full text-sm border text-gray-800 dark:text-gray-300 dark:border-gray-300 border-gray-800 ">
+                {darkMode?(<MdSunny />):(<IoMdMoon />)}
+            </button>
+            <Link to='/sign-up' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Register
+            </Link>
+            <Link to='/sign-in' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Login
+            </Link>
+          </div>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <NavLink to='/' className={navLinkClickHandler('/')}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/about' className={navLinkClickHandler('/about')}>
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/project' className={navLinkClickHandler('/project')}>
+                  Project
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/contact' className={navLinkClickHandler('/contact')}>
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
