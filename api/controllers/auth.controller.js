2779a623d1ae774370c14e1ed9224614732e2f7a
Akash-akp <akashakp0037@gmail.com>
2024-01-28 11:57:01 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async(req,res,next) =>{
     console.log(req.body);
     const {userName , email, password} = req.body;
     if(!userName || !email || !password || userName === '' || email === '' || password === '' ){
-        next(errorHandler(400,"All field are required"))
+        return next(errorHandler(400,"All field are required"))
     }
 
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -22,4 +23,35 @@ export const signup = async(req,res,next) =>{
     }catch(error){
         next(error);
     }
-}
+};
+
+export const signin = async(req,res,next) => {
+    console.log(req.body);
+    const {email , password} = req.body;
+    if(!email || !password){
+        return next(errorHandler(400,"All field are required"));
+    }
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(400,"Invalid Credential"));
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400,"Invalid Credential"));
+        }
+        // npm i jsonwebtoken
+        const token = jwt.sign(
+            { userId: validUser._id} , process.env.JWT_SECRET
+        );
+        const {password:pass , ...rest} = validUser._doc;
+        res
+        .status(200)
+        .cookie('access token',token,{
+            httpOnly:true,
+        })
+        .json(rest);
+    }catch(error){
+        next(error);
+    }
+};
